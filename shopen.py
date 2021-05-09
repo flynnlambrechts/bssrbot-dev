24592794e91150de3033cef90026f127f02c0d68
Flynn Lambrechts <flynnlambrechts@Flynns-MBP.modem>
2021-05-09 17:51:44 +1000

    handling errors

--- shopen.py
+++ shopen.py
@@ -66,17 +66,21 @@ def insert_shopen():
 
 
 def open_shopen():
-    global con
-    global person
-    global current_time, end_time, date
-    cur = con.cursor()
-    cur.execute('''UPDATE shopen SET
-        person= %s, start_time = %s, end_time = %s, value = %s,
-        date = %s''',
-            (person,current_time,end_time,'true',date))
-    print("Shopen updated successfully")
-    con.commit()
-    return "Shop has been opened"
+    try: 
+        global con
+        global person
+        global current_time, end_time, date
+        cur = con.cursor()
+        cur.execute('''UPDATE shopen SET
+            person= %s, start_time = %s, end_time = %s, value = %s,
+            date = %s''',
+                (person,current_time,end_time,'true',date))
+        print("Shopen updated successfully")
+        con.commit()
+        return "Shop has been opened"
+    except Exception as error:
+        return "fail"
+        #response = response + "Fail: " + str(type(error))
 
 def close_shopen():
     global con
@@ -98,33 +102,35 @@ def timeTillClose(end_time):
     return remaining_time
 
 def get_shopen():
-    #if before end time:
-    global con
-    cur = con.cursor()
-    cur.execute('''SELECT * FROM shopen''')
-    rows = cur.fetchall()
-    response = ""
-    for row in rows:
-        '''
-        print("person =", row[0])
-        print("start_time =", row[1])
-        print("end_time =", row[2])
-        print("value =", row[3])
-        print("date =", row[4], "\n")
-        '''
-        person = str(row[0])
-        start_time = row[1]
-        end_time = row[2]
-        value = str(row[3])
-        date = row[4]
-    
-    if timeTillClose(end_time) >= datetime.timedelta(minutes=0):
-        if value == "True":
-            response = response + "Yes, shop was opened by " + person + " at " + str(start_time.strftime('%I:%M %p')) + "."
-        elif value == "False":
+    try:
+        global con
+        cur = con.cursor()
+        cur.execute('''SELECT * FROM shopen''')
+        rows = cur.fetchall()
+        response = ""
+        for row in rows:
+            '''
+            print("person =", row[0])
+            print("start_time =", row[1])
+            print("end_time =", row[2])
+            print("value =", row[3])
+            print("date =", row[4], "\n")
+            '''
+            person = str(row[0])
+            start_time = row[1]
+            end_time = row[2]
+            value = str(row[3])
+            date = row[4]
+        
+        if timeTillClose(end_time) >= datetime.timedelta(minutes=0):
+            if value == "True":
+                response = response + "Yes, shop was opened by " + person + " at " + str(start_time.strftime('%I:%M %p')) + "."
+            elif value == "False":
+                response = response + "Sorry, shop closed :("
+        else:
             response = response + "Sorry, shop closed :("
-    else:
-        response = response + "Sorry, shop closed :("
+    except Exception as error:
+        response = response + "Fail: " + str(type(error))
     return response
 
 
