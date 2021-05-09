cb5d82dfb614a84d0f85c692525c22617e4e1ff7
Flynn Lambrechts <flynnlambrechts@Flynns-MBP.modem>
2021-05-09 15:49:44 +1000

    forgot to delete

--- shopen.py
+++ shopen.py
@@ -15,7 +15,6 @@ global person
 person = str("Wendy")
 
 global current_time, end_time, date
-unix = int(time.time(TIMEZONE))
 current_time = str(datetime.datetime.now(TIMEZONE).strftime('%H:%M:%S'))
 date_and_time = datetime.datetime.now(TIMEZONE)
 end_time = date_and_time + datetime.timedelta(hours=3) #closes shop after 3 hours
