f4c8d721d659589ebae66a3d36085c5ea4992e8f
Flynn Lambrechts <flynnlambrechts@Flynns-MBP.modem>
2021-05-09 19:08:41 +1000

    making so only ever be one row

--- shopen.py
+++ shopen.py
@@ -14,6 +14,9 @@ global con
 global person
 person = str("Wendy")
 
+global index
+index = int(1)
+
 global current_time, end_time, date
 '''
 current_time = datetime.datetime.now(TIMEZONE).strftime('%H:%M:%S')
@@ -32,8 +35,9 @@ def create_shopen():
     cur = con.cursor()
     response = ""
     try: 
-        cur.execute('''CREATE TABLE shopen
-            (person VARCHAR(50) NOT NULL PRIMARY KEY,
+        cur.execute('''CREATE TABLE shopen1
+            (index INTEGER NOT NULL PRIMARY KEY,
+            person VARCHAR(50) NOT NULL,
             start_time VARCHAR(50) NOT NULL,
             end_time VARCHAR(50) NOT NULL,
             value BOOLEAN NOT NULL,
@@ -50,14 +54,15 @@ def create_shopen():
 def insert_shopen():
     global con
     global person
+    global index
     global current_time, end_time, date
     response = ""
     try:
         cur = con.cursor()
-        cur.execute('''INSERT INTO shopen (
-            person, start_time, end_time, value, date)
-            VALUES (%s,%s,%s,%s,%s)''',
-                (person,current_time,end_time,'true',date))
+        cur.execute('''INSERT INTO shopen1 (
+            index, person, start_time, end_time, value, date)
+            VALUES (%s,%s,%s,%s,%s,%s)''',
+                (index, person,current_time,end_time,'true',date))
         print("Shopen data inserted successfully")
         response  = response + "Shop row inserted"
         con.commit()
@@ -71,15 +76,16 @@ def open_shopen():
     try: 
         global con
         global person
+        global index
         global current_time, end_time, date
         cur = con.cursor()
-        cur.execute('''UPDATE shopen SET
-            person= %s, start_time = %s, end_time = %s, value = %s,
+        cur.execute('''UPDATE shopen1 SET
+            index=%s, person= %s, start_time = %s, end_time = %s, value = %s,
             date = %s''',
-                (person,current_time,end_time,'true',date))
+                (index,person,current_time,end_time,'true',date))
         print("Shopen updated successfully")
         con.commit()
-        return "Shop has been opened"
+        return "Shop has been opened!"
     except Exception as error:
         print("Error: " + str(error) + "\n" + str(type(error)))
         return "Fail: " + str(error)
@@ -88,15 +94,16 @@ def close_shopen():
     try:
         global con
         global person
+        global index
         unix = int(time.time())
         current_time = str(datetime.datetime.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S'))
         cur = con.cursor()
-        cur.execute('''UPDATE shopen SET
-            person= %s, end_time = %s, value = %s''',
-                (person, current_time,'false'))
+        cur.execute('''UPDATE shopen1 SET
+            index=%s, person= %s, end_time = %s, value = %s''',
+                (index, person, current_time,'false'))
         print("Shopen updated successfully")
         con.commit()
-        return "Shop has been closed"
+        return "Shop has been closed!"
     except Exception as error:
         print("Error: " + str(error) + "\n" + str(type(error)))
         return "Fail: " + str(error)
@@ -112,7 +119,7 @@ def get_shopen():
     try:
         global con
         cur = con.cursor()
-        cur.execute('''SELECT * FROM shopen''')
+        cur.execute('''SELECT * FROM shopen1''')
         rows = cur.fetchall()
         response = ""
         for row in rows:
@@ -123,13 +130,13 @@ def get_shopen():
             print("value =", row[3])
             print("date =", row[4], "\n")
             '''
-            person = str(row[0])
-            start_time_string = row[1]
+            person = str(row[1])
+            start_time_string = row[2]
             start_time = datetime.datetime.strptime(str(start_time_string),'%Y-%m-%d %H:%M:%S')
-            end_time_string = row[2]
+            end_time_string = row[3]
             end_time = datetime.datetime.strptime(str(end_time_string),'%Y-%m-%d %H:%M:%S')
-            value = str(row[3])
-            date = row[4]
+            value = str(row[4])
+            date = row[5]
         
         if timeTillClose(end_time) >= datetime.timedelta(minutes=0):
             if value == "True":
