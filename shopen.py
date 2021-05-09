2ce2fff3341289b21e1c960d6a2d78bf33bfb364
Flynn Lambrechts <flynnlambrechts@Flynns-MBP.modem>
2021-05-09 18:17:05 +1000

    uhhh

--- shopen.py
+++ shopen.py
@@ -44,7 +44,7 @@ def create_shopen():
         con.commit()
     except Exception as error:
         response = response + "Fail: " + str(type(error))
-        print("Error:" + error)
+        print("Error:" + str(error))
     return response
 
 def insert_shopen():
@@ -63,7 +63,7 @@ def insert_shopen():
         con.commit()
     except Exception as error:
         response = response + "Fail: " + str(type(error))
-        print("Error:" + error)
+        print("Error:" + str(error))
     return response
 
 
@@ -81,7 +81,7 @@ def open_shopen():
         con.commit()
         return "Shop has been opened"
     except Exception as error:
-        print("Error:" + error)
+        print("Error:" + str(error))
         return "Fail: " + str(type(error))
 
 def close_shopen():
@@ -98,7 +98,7 @@ def close_shopen():
         con.commit()
         return "Shop has been closed"
     except Exception as error:
-        print("Error:" + error)
+        print("Error:" + str(error))
         return "Fail: " + str(type(error))
         
 
@@ -137,7 +137,7 @@ def get_shopen():
         else:
             response = response + "Sorry, shop closed :("
     except Exception as error:
-        print("Error:" + error)
+        print("Error:" + str(error))
         response = ""
         response = response + "Fail: " + str(type(error))
     return response
