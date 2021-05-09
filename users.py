7d1b62f3306a656eeb0af16f801071c115d5c2f5
Flynn Lambrechts <flynnlambrechts@Flynns-MBP.modem>
2021-05-10 00:45:25 +1000

    why is this so hard

--- users.py
+++ users.py
@@ -44,6 +44,7 @@ def insert_user(full_name, first_name, last_name, PSID):
         print("User may be already added: " + str(error) + "\n" + str(type(error)))
 
 def view_users():
+    connectToDB()
     response = ""
     global con
     try: 
@@ -63,20 +64,7 @@ def view_users():
             response = response + full_name + ", " + first_name + ", " + last_name + ", " + PSID
     except Exception as error:
         if str((error)) == "connection already closed":
-            connectToDB()
-            cur = con.cursor()
-            cur.execute('''SELECT * FROM users''')
-            rows = cur.fetchall()
-            for row in rows:
-                print("full_name =", row[0])
-                full_name = str(row[0])
-                print("first_name =", row[1])
-                first_name = str(row[1])
-                print("last_name =", row[2])
-                last_name = str(row[2])
-                print("PSID =", row[3], "\n")
-                PSID = str(row[3])
-                response = response + full_name + ", " + first_name + ", " + last_name + ", " + PSID
+            response = response + "con closed"
         else:
             print("Error Viewing: " + str(error) + "\n" + str(type(error)))
     return response
