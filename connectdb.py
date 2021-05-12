bb8b6e8083b726fdeb710453b58f0476b52be3a7
Flynn Lambrechts <flynnlambrechts@Flynns-MBP.modem>
2021-05-12 21:24:33 +1000

    test

--- connectdb.py
+++ connectdb.py
@@ -4,8 +4,9 @@ import psycopg2
 
 if "HEROKU" in os.environ:
     DATABASE_URL =  os.environ['DATABASE_URL']
-    db = (DATABASE_URL, sslmode='require')
-    con = psycopg2.connect(db) #DATABASE_URL, sslmode='require')
+    db = PostgresqlDatabase(DATABASE_URL, sslmode='require')
+    db.connect()
+    con = psycopg2.connect(DATABASE_URL, sslmode='require')
 else:
     con = psycopg2.connect(database="bssrbot1", user="flynnlambrechts", password="", host="127.0.0.1", port="5432")
     print("Local Database opened successfully")
