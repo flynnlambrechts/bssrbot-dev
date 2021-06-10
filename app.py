01abfdf607fe46cda2ab7625eb49ce146a5bfcba
Flynn Lambrechts <flynnlambrechts@Flynns-MacBook-Pro.local>
2021-06-10 11:39:33 +1000

    dunno

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ def receive_message():
         # get whatever message a user sent the bot
         output = request.get_json()
     try:
-        #log(output) #entire output good for finding sender ids what message contains etc
+        log(output) #entire output good for finding sender ids what message contains etc
         for event in output['entry']:
             messaging = event['messaging']
             for message in messaging:
