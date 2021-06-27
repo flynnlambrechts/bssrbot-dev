c8f69e43ecaaee4014a970759a550e82dd33ff85
Flynn Lambrechts <flynnlambrechts@Flynns-MacBook-Pro.local>
2021-06-27 21:26:39 +1000

    not working

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
+import re
 
-header = "salad"
-header = header.replace("salad", u"salad \U0001F957")
-print(header)
+def get_custom_message(message):
+	try:
+		custom_message = re.search("'(.+?)'", message).group(1)
+	except AttributeError:
+		custom_message = "no message"
+		print('no message found')
+	return custom_message
+message = "dookie: dinner 'hello'"
+print(get_custom_message(message))
