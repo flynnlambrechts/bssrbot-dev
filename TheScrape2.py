7c372d364c6d6f3a8bff3aa408bcf220202d07cc
Flynn Lambrechts <flynnlambrechts@Flynns-MacBook-Pro.local>
2021-06-28 14:18:31 +1000

    hmm

--- TheScrape2.py
+++ TheScrape2.py
@@ -39,7 +39,7 @@ dinotimes = "Dino Times: \nBreakfast: " + breakfasttime + "\nLunch: " + lunchtim
 
 
 def checkForDino(message, con, value):
-    start = datetime.now(TIMEZONE).time()
+    start = datetime.now(TIMEZONE).time().seconds
     print("checkForDino")
     #global current_day #day of week 0-6 inclusive
     #global day_value #day of week 1-7 inclusive
@@ -114,7 +114,7 @@ def checkForDino(message, con, value):
     if note is not None:
         response = response + str(note)
     print("checkForDino DONE")
-    end = datetime.now(TIMEZONE).time()
+    end = datetime.now(TIMEZONE).time().seconds
     print(str(end - start) + " checkForDino")
     return response
 
