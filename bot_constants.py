05dd68577f8199b488227c693c5717a2367a088a
flynnlambrechts <flynnlambrechts@Flynns-MacBook-Pro.local>
2021-07-08 21:29:11 +1000

    success

--- bot_constants.py
+++ bot_constants.py
@@ -2,7 +2,8 @@
 
 dino_quickreplies = [{'content_type': 'text', 'title': 'Breakfast', 'payload': 'Breakfast'},\
  {'content_type': 'text', 'title': 'Lunch', 'payload': 'Lunch'},\
- {'content_type': 'text', 'title': 'Dinner', 'payload': 'Dinner'}]
+ {'content_type': 'text', 'title': 'Dinner', 'payload': 'Dinner'},\
+ {'content_type': 'text', 'title': 'Dino', 'payload': 'Dino'}]
 
 
 
