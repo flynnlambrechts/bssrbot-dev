9ed9c78a6781e94a7ad048d647512c7326b54f9e
flynnlambrechts <flynn.lambrechts@gmail.com>
2021-07-13 23:19:13 +1000

    gorgot to redefine access tokeN

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ import requests
 from users import insert_user
 from bot_functions import (PrintException, getCon)
 
-#ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 
 class Sender:
 	def __init__(self, recipient_id):
