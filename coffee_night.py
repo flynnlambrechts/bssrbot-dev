9bb76927807f2246c33f2c80d9e59a2bde31128f
flynnlambrechts <flynn.lambrechts@gmail.com>
2021-09-30 18:26:36 +1000

    WORKING IN finding out who nomiated

--- coffee_night.py
+++ coffee_night.py
@@ -3,14 +3,19 @@
 #Quote Nominations
 #Photos
 
+from models import Sender
+
 #--- Wildcat Nominations
 def add_nomination(rs, args): 
 # Recieves a list of words containing the persons name first followed by the reason
 # E.g. ["Flynn", "for", "making", "BssrBot"]
-	person = args[0]
+	nominee = args[0]
 	reason = " ".join(args[1:])
-	print(Person + "For" + reason)
-	return args
+
+	psid = bot.current_user()
+	person = Sender(psid).get_fullname()
+
+	print(nomiee + " For " + reason + " By " + person)
 
 #--- Quote Submission
 def add_quote(rs, args):
@@ -19,3 +24,18 @@ def add_quote(rs, args):
 
 #--- Photo Submission
 
+
+
+
+# def set_vacuum(rs, location):
+#     try:
+#         psid = bot.current_user()
+#         location = " ".join(location)
+#         person = Sender(psid).get_fullname()
+#         time_now = datetime.datetime.now(TIMEZONE)
+#         print(time_now)
+#         GlobalVar('vacuum').update({'index':1,'location':location,'person':person,'time':time_now})
+#         return "Hope you had a good 'cuum. The location has been updated"
+#     except:
+#         PrintException()
+#     
