49f0037ba8e38a96869b0a07463f61aab1e40d23
flynnlambrechts <flynn.lambrechts@gmail.com>
2021-09-30 20:14:52 +1000

    not suprised

--- coffee_night.py
+++ coffee_night.py
@@ -4,28 +4,30 @@
 #Photos
 import datetime
 from bot_constants import TIMEZONE
-from bot_functions import getCon
+from bot_functions import (getCon, PrintException)
 
 from models import Sender
 
 # Getting qutoes or wildcat nominations
 # Inputing into here is done through rive_reply.py
 def get_coffee(item): #item is either quotes or wildcats
-	date = datetime.datetime.now(TIMEZONE).strftime('%Y-%m-%d')
+	try:
+		date = datetime.datetime.now(TIMEZONE).strftime('%Y-%m-%d')
 
-	con = getCon()
-	cur = con.cursor()
-	cur.execute('''SELECT * FROM %s WHERE date >= "2021-09-29"::DATE''', (item))
-	
-	rows = cur.fetchall()
-	for row in rows:
-		for i in row:
-			print(row[i])
-			print(" ")
-		print("\n")
+		con = getCon()
+		cur = con.cursor()
+		cur.execute('''SELECT * FROM %s WHERE date >= "2021-09-29"::DATE''', (item))
+		
+		rows = cur.fetchall()
+		for row in rows:
+			for i in row:
+				print(row[i])
+				print(" ")
+			print("\n")
 
-	con.close()
-
+		con.close()
+	except:
+		PrintException()
 	return 0
 
 
