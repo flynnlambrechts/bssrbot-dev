556a885123a0060387b32ecc5a3a3e7c37bc6391
flynnlambrechts <flynn.lambrechts@gmail.com>
2021-11-02 23:11:10 +1100

    print

--- bot_response.py
+++ bot_response.py
@@ -28,6 +28,7 @@ def bot_response(recipient_id, message_text="", attachment = ""):
 		response = Response(recipient_id)
 		picture = Response(recipient_id)
 		if attachment != "":
+			print(attachment[0]['payload']['sticker_id'])
 			if attachment[0]['payload']['sticker_id'] == "369239263222822":
 				response.text = "Thumbs up to you too 👍"
 			else:
